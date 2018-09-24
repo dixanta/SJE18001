d43fc7cc7a2c3c9baf63ecc13c10fbf1f5473333
dixanta <dixanta@gmail.com>
2018-09-24 09:53:07 +0545

    First commit

---
+++ com/cibt/day1/Program.java
@@ -0,0 +1,16 @@
+package com.cibt.day1;
+
+import com.cibt.day1.util.Calculator;
+
+public class Program{
+
+	public static void main(String[] args){
+		
+		Calculator c=new Calculator();
+
+		System.out.println(c.add(3,4));
+
+		c=null;
+	}
+} 
+
---
+++ com/cibt/day1/util/Calculator.java
@@ -0,0 +1,16 @@
+package com.cibt.day1.util;
+
+
+public class Calculator{
+
+    static int abc=10;
+
+    public int add(int x, int y){
+        return x+y+ abc;
+    }
+
+    public static void test(){
+        System.out.println("this is test");
+        System.out.println(abc);
+    }
+}
